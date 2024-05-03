eb23e3a7cf55f14f8127146d4399816c0cc12c5f
broshet44 <broshet44@gmail.com>
2024-05-03 15:54:44 +0200

    Implémentation d'un Signin Google

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { environment } from "../environments/environment.development";
+import { SigninComponent } from './signin/signin.component';
+import { LandingComponent } from './landing/landing.component';
+import { GoogleSsoDirective } from './google-sso.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    LandingComponent,
+    GoogleSsoDirective
+  ],
+  imports: [
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),    
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false, 
+    firebaseConfig: {
+        apiKey: "AIzaSyBx0x0cg9QY3TO6Er082eXZlUa0tikp2-o",
+        authDomain: "puyfolais-app.firebaseapp.com",
+        projectId: "puyfolais-app",
+        storageBucket: "puyfolais-app.appspot.com",
+        messagingSenderId: "502516336689",
+        appId: "1:502516336689:web:e1107fca1d17f2f8266d10"
+    }
+};
