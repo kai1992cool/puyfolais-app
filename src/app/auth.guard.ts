953a8ccba1d1a80de5b5a5c626208b80f21364e8
broshet44 <broshet44@gmail.com>
2024-05-03 16:06:15 +0200

    Ajout de la redirection si non authentifié

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,16 @@
 import { CanActivateFn } from "@angular/router";
 import { inject } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { Router } from "@angular/router";
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const angularFireAuth = inject(AngularFireAuth);
+  const router = inject(Router);
   const user = await angularFireAuth.currentUser;
   // coerce to boolean
   const isLoggedIn = !!user;
+  if (!isLoggedIn) {
+    router.navigateByUrl('/signin');
+  }
   return isLoggedIn;
 };
